8a91bac8834beca76e21ad511f15da46e0122393
tekkukerli <kerlitekku@gmail.com>
2019-12-04 15:47:04 +0200

    vormist andmed muutujasse

--- vormid/tellimine.php
+++ vormid/tellimine.php
@@ -1,2 +1,10 @@
 <?php
 echo "<h1>Tellimine</h1>";
+//lisab vormist saadud andmed muutujasse
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
+
+echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Toode 2: '.$toode2.'tk<br>';
+echo 'Toode 3: '.$toode3.'tk<br>';
